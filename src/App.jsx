3f8c9c23d57466f8039bc6bdcb7a9f5a9e06cbf7
gallant4649 <gallantfathurrahman@gmail.com>
2025-09-29 14:37:44 +0700

    Add main application structure and page components

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,22 @@
 import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 
+import HomePage from "./Pages/HomePage";
+import ProfilePage from "./Pages/ProfilePage";
+import FilmPage from "./Pages/FilmPage";
+import SeriesPage from "./Pages/SeriesPage";
+import TrendingPage from "./Pages/TrendingPage";
+
 function App() {
   return (
-    <div className="">
-      <Router>
-        <Routes>
-          <Route path="/" element={<HomePage />} />
-          <Route path="/profile" element={<ProfilePage />} />
-          <Route path="/film" element={<FilmPage />} />
-          <Route path="/series" element={<SeriesPage />} />
-          <Route path="/trending" element={<TrendingPage />} />
-        </Routes>
-      </Router>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/profile" element={<ProfilePage />} />
+        <Route path="/film" element={<FilmPage />} />
+        <Route path="/series" element={<SeriesPage />} />
+        <Route path="/trending" element={<TrendingPage />} />
+      </Routes>
+    </Router>
   );
 }
 
