c895c15ab30aabba2f9cc670d2c06d7d9d2e73d4
gallant4649 <gallantfathurrahman@gmail.com>
2025-09-30 11:00:52 +0700

    Add HeroSlider and TrendingSection components; implement image slides and navigation controls

--- src/Pages/HomePage.jsx
+++ src/Pages/HomePage.jsx
@@ -1,9 +1,11 @@
 import HeroSection from "../components/HeroSection";
+import TrendingSection from "../components/TrendingSection";
 
 export default function HomePage() {
   return (
     <div className="">
       <HeroSection />
+      <TrendingSection />
     </div>
   );
 }
