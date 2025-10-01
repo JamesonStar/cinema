8b07865108c65f7e52f71f007ebba37dc24d0bb7
Andre <agustinuss.andre08@gmail.com>
2025-10-01 15:13:22 +0700

    herosection

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -40,55 +40,50 @@ export default function HeroSlider() {
 
   return (
     <div className="container relative text-text">
-      {/* Wrapper */}
       <div className="relative h-40 lg:h-60 rounded-lg overflow-hidden">
-        {/* Track */}
-        <div
-          className="flex h-full transition-transform duration-700 ease-in-out"
-          style={{ transform: `translateX(-${current * 100}%)` }}
-        >
-          {slides.map((slide) => (
-            <div
-              key={slide.id}
-              className="w-full flex-shrink-0 relative bg-cover bg-center"
-              style={{ backgroundImage: `url(${slide.bg})` }}
-            >
-              {/* Overlay */}
-              <div className="absolute inset-0 bg-black/50 flex items-center gap-2 p-2">
-                {/* Poster */}
-                <div className="p-2 w-28 lg:w-40 shrink-0">
-                  <img
-                    className="rounded-md"
-                    src={slide.poster}
-                    alt={slide.title}
-                  />
-                </div>
+        {slides.map((slide, idx) => (
+          <div
+            key={slide.id}
+            className={`absolute inset-0 bg-cover bg-center transition-opacity duration-500 ${
+              current === idx ? "opacity-100" : "opacity-0"
+            }`}
+            style={{ backgroundImage: `url(${slide.bg})` }}
+          >
+            {/* Overlay */}
+            <div className="absolute inset-0 bg-black/50 flex items-center gap-2 p-2 transition-all">
+              {/* Poster */}
+              <div className="p-2 w-28 lg:w-40 shrink-0">
+                <img
+                  className="rounded-md"
+                  src={slide.poster}
+                  alt={slide.title}
+                />
+              </div>
 
-                {/* Info */}
-                <div className="flex flex-col gap-2 max-w-lg pb-5">
-                  <div className="flex gap-1 items-center text-sm">
-                    <p className="text-[7px] bg-yellow-600 w-7 h-3 text-center rounded-sm text-black">
-                      Tmdb
-                    </p>
-                    <p className="text-[10px]">{slide.rating}</p>
-                  </div>
-                  <p className="text-sm font-bold lg:text-2xl">{slide.title}</p>
-                  <p className="text-[10px] font-extralight lg:text-sm">
-                    {slide.desc}
+              {/* Info */}
+              <div className="flex flex-col gap-2 max-w-lg pb-5">
+                <div className="flex gap-1 items-center text-sm">
+                  <p className="text-[7px] bg-yellow-600 w-7 h-3 text-center rounded-sm text-black">
+                    Tmdb
                   </p>
-                  <div className="flex pt-2 gap-1">
-                    <button className="bg-secondary rounded-sm p-1 text-[10px] border border-transparent hover:border-secondary hover:bg-black/50 hover:text-secondary transition-all">
-                      See Detail
-                    </button>
-                    <button className="rounded-sm font-bold bg-linear-60/oklch from-secondary via-accent bg-clip-text text-transparent to-highlight p-1 text-[10px] border border-transparent hover:bg-clip-border hover:bg-black/30 hover:text-white transition-all">
-                      Add Watchlist
-                    </button>
-                  </div>
+                  <p className="text-[10px]">{slide.rating}</p>
+                </div>
+                <p className="text-sm font-bold lg:text-2xl">{slide.title}</p>
+                <p className="text-[10px] font-extralight lg:text-sm">
+                  {slide.desc}
+                </p>
+                <div className="flex pt-2 gap-1">
+                  <button className="bg-secondary rounded-sm p-1 text-[10px] border border-transparent hover:border-secondary hover:bg-black/50 hover:text-secondary transition-all">
+                    See Detail
+                  </button>
+                  <button className="rounded-sm font-bold bg-linear-60/oklch from-secondary via-accent bg-clip-text text-transparent to-highlight p-1 text-[10px] border border-transparent hover:bg-clip-border hover:bg-black/30 hover:text-white transition-all">
+                    Add Watchlist
+                  </button>
                 </div>
               </div>
             </div>
-          ))}
-        </div>
+          </div>
+        ))}
 
         {/* Controls */}
         <button
