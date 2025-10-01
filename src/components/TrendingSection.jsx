e4138f5228c9fa5d4b8e364b1dad6f2fe846e052
gallant4649 <gallantfathurrahman@gmail.com>
2025-10-01 11:08:08 +0700

    Implement TrendingSection layout with scrollable container and item cards

--- src/components/TrendingSection.jsx
+++ src/components/TrendingSection.jsx
@@ -1,7 +1,9 @@
 export default function TrendingSection() {
-    return(
-        <div className="container text-text">
-
-        </div>
-    )
+  return (
+    <div className="container flex gap-4 overflow-x-scroll">
+      <div className="w-40 h-50 bg-secondary shrink-0 rounded-lg"></div>
+      <div className="w-40 h-50 bg-secondary shrink-0 rounded-lg"></div>
+      <div className="w-40 h-50 bg-secondary shrink-0 rounded-lg"></div>
+    </div>
+  );
 }
